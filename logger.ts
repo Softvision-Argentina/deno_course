e168c5dac7c8d602284ee53d9f777bf6203ad545
Víctor Hugo Valle Castillo <victouk@gmail.com>
2019-09-21 19:24:56 -0300

    Moving logger script outside its dependencies.

---
+++ logger.ts
@@ -0,0 +1,10 @@
+import { logger } from './logger/logger_v1.ts';
+
+
+(async () => {
+    const logger2 = await import('./logger/logger_v2.ts');
+
+    logger('hello world!');
+
+    logger2.logger('hey!');
+})();
