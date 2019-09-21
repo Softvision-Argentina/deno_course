4b8492e57b0507465866536888d41b6d8e7af65b
Víctor Hugo Valle Castillo <victouk@gmail.com>
2019-09-21 19:29:46 -0300

    Fixing text indentation into files after adding the .editorconfig file.

--- logger.ts
+++ logger.ts
@@ -2,9 +2,9 @@ import { logger } from './logger/logger_v1.ts';
 
 
 (async () => {
-    const logger2 = await import('./logger/logger_v2.ts');
+  const logger2 = await import('./logger/logger_v2.ts');
 
-    logger('hello world!');
+  logger('hello world!');
 
-    logger2.logger('hey!');
+  logger2.logger('hey!');
 })();
--- logger/logger_v1.ts
+++ logger/logger_v1.ts
@@ -4,5 +4,5 @@ import { bgBlue, red, bold } from 'https://deno.land/std/fmt/colors.ts';
 const logger = (text) => console.log(bgBlue(red(bold(text))));
 
 export {
-    logger,
+  logger,
 };
