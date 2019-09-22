84f9ed8f5e98c2653330e2a7e7b4115b89e1fb31
Víctor Hugo Valle Castillo <victouk@gmail.com>
2019-09-21 21:25:30 -0300

    Cleaning up the deprecated module after being moved to a public project.

--- logger/logger_v1.ts
+++
@@ -1,8 +0,0 @@
-import { bgBlue, red, bold } from 'https://deno.land/std/fmt/colors.ts';
-
-
-const logger = (text) => console.log(bgBlue(red(bold(text))));
-
-export {
-  logger,
-};
--- logger/logger_v2.ts
+++
@@ -1,8 +0,0 @@
-import { bgBlue, red, bold, italic } from 'https://deno.land/std/fmt/colors.ts';
-
-
-const logger = (text: string) => console.log(bgBlue(italic(red(bold(text)))));
-
-export {
-  logger,
-};
