68cd9cd4eec979785edbe8240a66d0ef979b63cc
Víctor Hugo Valle Castillo <victouk@gmail.com>
2019-09-23 22:22:15 -0300

    Adding divide operation within the math operations script.

--- math_operations.ts
+++ math_operations.ts
@@ -5,6 +5,7 @@ import {
   add,
   subtract,
   multiply,
+  divide,
 } from 'https://vihuvac.github.io/features/deno/math/operations_v1.ts';
 
 
@@ -16,10 +17,12 @@ import {
   const addOp = add(2, 2);
   const subtractOp = subtract(4, 2);
   const multiplyOp = multiply(4, 4);
+  const divideOp = divide(10, 2);
 
   // Print results.
   console.log('Printing the results of the operations:');
   console.log('add(2, 2) =>', addOp);
   console.log('subtract(4, 2) =>', subtractOp);
   console.log('multiply(4, 4) =>', multiplyOp);
+  console.log('divide(10, 2) =>', divideOp);
 })();
