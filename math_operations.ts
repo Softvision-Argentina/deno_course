885032a07ff92920c809d7dbf889a26faf7de4e8
Víctor Hugo Valle Castillo <victouk@gmail.com>
2019-09-23 22:20:58 -0300

    Adding multiply operation within the math operations script.

--- math_operations.ts
+++ math_operations.ts
@@ -4,6 +4,7 @@
 import {
   add,
   subtract,
+  multiply,
 } from 'https://vihuvac.github.io/features/deno/math/operations_v1.ts';
 
 
@@ -14,9 +15,11 @@ import {
   // Invoke operation methods.
   const addOp = add(2, 2);
   const subtractOp = subtract(4, 2);
+  const multiplyOp = multiply(4, 4);
 
   // Print results.
   console.log('Printing the results of the operations:');
   console.log('add(2, 2) =>', addOp);
-  console.log('subtractOp(4, 2) =>', subtractOp);
+  console.log('subtract(4, 2) =>', subtractOp);
+  console.log('multiply(4, 4) =>', multiplyOp);
 })();
