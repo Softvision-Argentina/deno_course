6ee6554bb90720e80abb3cca38b9d1fa00076822
Víctor Hugo Valle Castillo <victouk@gmail.com>
2019-09-23 22:46:15 -0300

    Importing the add method from the new operations library.

--- math_operations.ts
+++ math_operations.ts
@@ -7,6 +7,10 @@ import {
   multiply,
   divide,
 } from 'https://vihuvac.github.io/features/deno/math/operations_v1.ts';
+import {
+  add as adder,
+} from 'https://vihuvac.github.io/features/deno/math/operations_v2.ts';
+import { bgYellow, gray, bold } from 'https://deno.land/std/fmt/colors.ts';
 
 
 /**
@@ -18,6 +22,7 @@ import {
   const subtractOp = subtract(4, 2);
   const multiplyOp = multiply(4, 4);
   const divideOp = divide(10, 2);
+  const adderOp = adder(2, 2);
 
   // Print results.
   console.log('Printing the results of the operations:');
@@ -25,4 +30,5 @@ import {
   console.log('subtract(4, 2) =>', subtractOp);
   console.log('multiply(4, 4) =>', multiplyOp);
   console.log('divide(10, 2) =>', divideOp);
+  console.log(bgYellow(gray(bold(`adder(2, 2) => ${adderOp}`))));
 })();
